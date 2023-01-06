8d35070e510da465f411e05d86cfd6c618ca16b9
BT_3W2_29 <raymondg89@Nycstudents.net>
2023-01-06 08:45:56 -0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,33 @@
 public class Main {
     public static void main(String args[]){
-        String[] myStrs = {"hi", "bye", "what", "purple"};
-        int arrLen = myStrs.length;
-        int strLen = myStrs[3].length();
-        System.out.println(arrLen + strLen);
+        int[] nums1 = {5, 8, 9, 10, 4};
+        int numStr1 = 5;
+        ArrayManipulator manipulator = new ArrayManipulator(nums1, numStr1);
+        System.out.println("------------");
+        System.out.println(manipulator.getNumsMiddleIndex());
+        System.out.println(manipulator.getNumsAverage());
+        System.out.println(manipulator.sameLengths());
+        System.out.println(manipulator.getNumAt(3));
+        System.out.println(manipulator.getWordAt(3));
+        System.out.println(manipulator.updateWordsListAt(3, "hello"));
+        System.out.println(manipulator.getWordAt(3));
+        System.out.println(manipulator.howLongLastWord());
+        System.out.println(manipulator.updateWordsListAt(4, "updated"));
+        System.out.println(manipulator.howLongLastWord());
 
-        String str = myStrs[2].substring(1, 3);
-        System.out.println(str);
+
+        int[] nums2 = {2, 5, 7, 9,};
+        int numStr2 = 6;
+        System.out.println("------------");
+        manipulator = new ArrayManipulator(nums2, numStr2);
+        System.out.println(manipulator.getNumsMiddleIndex());
+        System.out.println(manipulator.getNumsAverage());
+        System.out.println(manipulator.sameLengths());
+        System.out.println(manipulator.getNumAt(3));
+        System.out.println(manipulator.updateWordsListAt(6, "not valid"));
+        System.out.println(manipulator.howLongLastWord());
+        System.out.println(manipulator.updateWordsListAt(5, "valid"));
+        System.out.println(manipulator.howLongLastWord());
+
     }
 }
